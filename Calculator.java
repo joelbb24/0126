33ace8104be9ad9f6860606baff7518bd8b93f49
joelbb24 <joelanilraj2410@gmail.com>
2021-01-28 00:19:25 -0500

    Initial commit

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+
+
+public class Calculator
+{
+    public int  subtraction(int a, int b)
+    {
+        return a-b;
+    }
+
+    public double subtraction(double a, double b)
+    {
+        return a-b;
+    }
+
+    public long subtraction(long a,long b)
+    {
+        return a-b;
+    }
+}
---
+++ CalculatorMain.java
@@ -0,0 +1,13 @@
+
+
+
+public class CalculatorMain
+{
+    public static void main(String[] args)
+    {
+        Calculator clc = new Calculator();
+        System.out.println(clc.subtraction(5,4));
+        System.out.println(clc.subtraction(6.3,3.2));
+        System.out.println(clc.subtraction(1000000000,499999));
+    }
+}
